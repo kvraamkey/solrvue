5a13a247453dabe331c9bfe74f9d61dc6d65250b
Ramki A <kvraamkeydev@gmail.com>
2021-05-23 00:17:09 +0530

    simple layout enabled

--- src/pages/app.svelte
+++ src/pages/app.svelte
@@ -1,8 +1,15 @@
-<!-- App.svelte -->
-<div class="App">
-  <header class="App-header">
-    <a class="App-link" href="https://svelte.dev" target="_blank" rel="noopener noreferrer">
-      Learn Svelte
-    </a>
-  </header>
-</div>
+<section class="flex flex-col w-full h-full">
+    <header class="flex-initial flex justify-between px-2 pt-1 pb-1 border-b">
+        <div class="flex-initial">
+            <img width="70" src="https://solr.apache.org/theme/images/identity/Solr_Logo_on_white.png" alt="">
+        </div>
+        <div class="flex items-center justify-center">
+            <svg class="w-8 h-8" viewBox="0 0 32 32" xmlns="http://www.w3.org/2000/svg" aria-hidden="true" role="presentation" focusable="false">
+                <path d="m16 .7c-8.437 0-15.3 6.863-15.3 15.3s6.863 15.3 15.3 15.3 15.3-6.863 15.3-15.3-6.863-15.3-15.3-15.3zm0 28c-4.021 0-7.605-1.884-9.933-4.81a12.425 12.425 0 0 1 6.451-4.4 6.507 6.507 0 0 1 -3.018-5.49c0-3.584 2.916-6.5 6.5-6.5s6.5 2.916 6.5 6.5a6.513 6.513 0 0 1 -3.019 5.491 12.42 12.42 0 0 1 6.452 4.4c-2.328 2.925-5.912 4.809-9.933 4.809z"></path>
+            </svg>
+        </div>
+    </header>
+    <section class="flex-1 p-3">
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Non nihil excepturi libero error id nulla molestias eaque, earum eum quisquam provident unde, molestiae voluptate culpa atque officiis quia ducimus nobis?
+    </section>
+</section>
