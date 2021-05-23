65abfc935de4314e9c9cac3f010175cae8503c90
Ramki A <kvraamkeydev@gmail.com>
2021-05-24 00:17:11 +0530

    login screnn navigatins

--- src/_components/Header.svelte
+++ src/_components/Header.svelte
@@ -1,10 +1,13 @@
 <script>
-    import { url } from '@src/_store';
+    import store from '@src/_store';
     import { toCamelCase } from '@src/_utils';
     function navigation(e) {
         let comp = e.target.dataset.url;
         window.history.replaceState(null, null, `?p=${comp}`);
-        url.update((u) => toCamelCase(comp));
+        store.update((self) => {
+            self.activePage = toCamelCase(comp);
+            return self;
+        });
     }
 
 </script>
@@ -14,10 +17,18 @@
         <img class="w-8" src="./logo.png" alt="" />
         <span class="text-xl tracking-widest">Solr-Vue</span>
     </div>
+    <!-- <div class="relative">
+        <div class="flex flex-wrap items-stretch w-full relative">
+            <input type="text" class="flex-1 border h-10 rounded px-3 relative" placeholder="Recipient's username" />
+            <div class="flex -mr-px">
+                <span class="flex items-center bg-gray-400 rounded rounded-l-none border border-l-0 px-3">Connect</span>
+            </div>
+        </div>
+    </div> -->
     <div class="flex items-center justify-center">
         <div class="flex space-x-4">
             <button on:click={navigation} data-url={'dashboard'} class="text-blue-600 hover:underline px-3 py-2 cursor-pointer">Dashboard</button>
-            <button on:click={navigation} data-url={"collections"} class="text-blue-600 hover:underline px-3 py-2 cursor-pointer">Collections</button>
+            <button on:click={navigation} data-url={'collections'} class="text-blue-600 hover:underline px-3 py-2 cursor-pointer">Collections</button>
             <span class="text-blue-600 hover:underline px-3 py-2 cursor-pointer">Logout</span>
         </div>
     </div>
--- src/pages/app.svelte
+++ src/pages/app.svelte
@@ -1,18 +1,54 @@
 <script>
     import { onMount } from 'svelte';
-    import { url } from '@src/_store';
+    import store from '@src/_store';
     import * as Components from '@src/pages';
     import { getUrlParam, toCamelCase } from '@src/_utils';
     import Header from '@components/Header.svelte';
     onMount(async () => {
-        url.update((u) => (getUrlParam('p') ? toCamelCase(getUrlParam('p')) : 'Dashboard'));
+        const sessionStarted = JSON.parse(sessionStorage.getItem('sessionStarted')) || false;
+        const activePage = getUrlParam('p') || 'dashboard'
+        store.update((self) => {
+            self.isConnected = sessionStarted;
+            self.activePage = toCamelCase(activePage);
+            return self;
+        });
     });
+
+    function navigation() {
+        let comp = 'collections';
+        window.history.replaceState(null, null, `?p=${comp}`);
+        sessionStorage.setItem('sessionStarted', true);
+        store.update((self) => {
+            self.activePage = toCamelCase(comp);
+            self.isConnected = true;
+            return self;
+        });
+    }
 
 </script>
 
 <section class="flex flex-col w-full h-full bg-gray-100">
-    <Header />
-    <section class="flex-1 p-3">
-        <svelte:component this={$url ? Components[$url] : Components['Dashboard']} />
+    {#if $store.isConnected}
+        <Header />
+    {/if}
+    <!-- <pre>{JSON.stringify($store, null, 2)}</pre> -->
+    <section class="flex-1 p-3 flex items-center justify-center">
+        {#if !$store.isConnected}
+            <div class="bg-white w-1/3 shadow-xl rounded p-5">
+                <img class="w-8" src="./logo.png" alt="" />
+                <h1 class="text-2xl">Welcome to SolrVue</h1>
+                <p class="text-xs">Apache Solr gui for the browser</p>
+                <div class="space-y-5 mt-5" autocomplete="off">
+                    <input type="text" class="w-full h-10 border border-gray-400 rounded px-3" placeholder="url" />
+                    <div class="flex space-x-3">
+                        <input type="text" class="w-full h-10 border border-gray-400 rounded px-3" placeholder="username" />
+                        <input type="password" class="w-full h-10 border border-gray-400 rounded px-3" placeholder="password" />
+                    </div>
+                    <button on:click={navigation} class="text-center w-full bg-blue-900 rounded-md text-white py-3">Connect</button>
+                </div>
+            </div>
+        {:else}
+            <svelte:component this={Components[$store.activePage]} />
+        {/if}
     </section>
 </section>
