aca96008357f51ccb34609d89aee7d1f805410c9
Ramki A <kvraamkeydev@gmail.com>
2021-05-29 21:27:28 +0530

    server modificatiion

--- server.js
+++ server.js
@@ -1,30 +1,5 @@
-const express = require('express');
-const got = require('got');
-const cors = require('cors');
-const app = express();
+'use strict';
 
-app.use(cors());
+const app = require('./server');
 
-app.get('/get', async (req, res) => {
-    const { solr_url, solr_username, solr_password } = req.headers;
-    try {
-        var Authorization = Buffer.from(solr_username + ':' + solr_password).toString('base64');
-        const resp = await got(`${solr_url}admin/collections?action=LIST`, {
-            headers: { Authorization: `Basic ${Authorization}` }
-        }).json();
-        res.json(resp)
-    } catch (error) {
-        res.json({ error: true, message: error.message })
-    }
-});
-
-app.use(express.static('dist'));
-app.use('*', (req, res) => {
-    res.sendFile(path.resolve(__dirname, 'dist', 'index.html'));
-});
-
-const port = 5555;
-
-app.listen(port, () => {
-    console.log(`Server is up at port ${port}`);
-});
+app.listen(3000, () => console.log('Local app listening on port 3000!'));
