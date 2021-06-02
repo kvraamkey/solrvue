09b784ab7761de250ce24f964b9b0288c6224a53
Ramki A <kvraamkeydev@gmail.com>
2021-06-02 22:55:50 +0530

    table modification

--- src/views/Collections.svelte
+++ src/views/Collections.svelte
@@ -71,42 +71,11 @@
             </div>
         </header>
         <div class="flex-1 flex flex-col w-full h-full overflow-hidden">
-            <div class="w-full flex-initial">
-                <div class="block relative">
-                    <span class="h-full absolute inset-y-0 left-0 flex items-center pl-2">
-                        <svg viewBox="0 0 24 24" class="h-4 w-4 fill-current text-gray-500">
-                            <path d="M10 4a6 6 0 100 12 6 6 0 000-12zm-8 6a8 8 0 1114.32 4.906l5.387 5.387a1 1 0 01-1.414 1.414l-5.387-5.387A8 8 0 012 10z" />
-                        </svg>
-                    </span>
-                    <input placeholder="Search" class="appearance-none border-b block pl-8 pr-6 py-2 w-full bg-white text-sm placeholder-gray-400 text-gray-700 focus:bg-white focus:placeholder-gray-600 focus:text-gray-700 focus:outline-none" />
-                </div>
-            </div>
-            <div class="flex-1 overflow-auto">
-                <div class="table w-full">
-                    <div class="table-header-group">
-                        <div class="table-row">
-                            <div class="table-cell sticky top-0 p-1 shadow shadow-dark-700 font-medium">Items</div>
-                            <div class="table-cell sticky top-0 p-1 shadow shadow-dark-700 font-medium">Expenditure</div>
-                        </div>
-                    </div>
-                    <div class="table-row-group">
-                        <div class="table-row">
-                            <div class="table-cell border-b p-1.5">Donuts</div>
-                            <div class="table-cell border-b p-1.5">3,000</div>
-                        </div>
-                        <div class="table-row">
-                            <div class="table-cell border-b p-1.5">Stationery</div>
-                            <div class="table-cell border-b p-1.5">18,000</div>
-                        </div>
-                    </div>
-                </div>
-            </div>
-            <div class="flex-initial bg-gray-100 p-2">
-                <div class="table-row">
-                    <div class="table-cell text-emerald-600">Totals</div>
-                    <div class="table-cell text-emerald-600">21,000</div>
-                </div>
-            </div>
+            <section class="t-wr flex h-full w-full">
+                <div class="t-h"></div>
+                <div class="t-b"></div>
+                <div class="t-f"></div>
+            </section>
         </div>
         <!-- <Error message="Please select a collection or alias" /> -->
     </div>
